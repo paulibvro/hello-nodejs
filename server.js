e51debdd0ed35158d6cacdd057bea9db871e99ab
Prediger Paul <office@spacetime.ro>
2017-01-03 11:21:33 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+
+
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for '${req.url}'-${JSON.stringify(req.body)}`);
+	next();
+});
+
+app.use(express.static("./public"));
+
+app.use(cors());
+
+app.get("/", function(req, res) {
+
+    res.json('SUP');
+
+});
+
+
+app.listen(3001);
+
+console.log("Express app running on port 3001");
+
+module.exports = app;
